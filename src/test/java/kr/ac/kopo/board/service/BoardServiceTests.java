8417be3c75d3bc5b4a895ae2dc6f1a92e2637891
shinjaesang <tlswltkd1011@gmail.com>
2024-10-04 08:58:49 +0900

    수정기능

--- src/test/java/kr/ac/kopo/board/service/BoardServiceTests.java
+++ src/test/java/kr/ac/kopo/board/service/BoardServiceTests.java
@@ -1,6 +1,9 @@
 package kr.ac.kopo.board.service;
 
+
 import kr.ac.kopo.board.dto.BoardDTO;
+import kr.ac.kopo.board.dto.PageRequestDTO;
+import kr.ac.kopo.board.dto.PageResultDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,11 +17,49 @@ public class BoardServiceTests {
     @Test
     public void testRegister(){
         BoardDTO dto = BoardDTO.builder()
-                .title("Board Test.....101")
+                .title("101 Board Test...")
                 .content("101 Board Test Board Test Board Test")
                 .writerEmail("user7@kopo.ac.kr")
                 .build();
         Long bno = boardService.register(dto);
-        System.out.println("정상적으로 글이 저장되었습니다." + bno);
+        System.out.println("정상적으로 글이 저장되었습니다. : "+bno);
+    }
+
+    @Test
+    public void testList(){
+        PageRequestDTO pageRequestDTO  = new PageRequestDTO();
+
+        PageResultDTO<BoardDTO, Object[]> result = boardService.getList(pageRequestDTO);
+
+        for (BoardDTO boardDTO : result.getDtoList()){
+            System.out.println(boardDTO);
+        }
+
+    }
+
+    @Test
+    public void testGet(){
+        Long bno = 1L;
+
+        BoardDTO boardDTO = boardService.get(bno);
+
+        System.out.println(boardDTO);
+    }
+
+    @Test
+    public void testRemove(){
+        Long bno = 1L;
+        boardService.removeWithReplies(bno);
+    }
+
+    @Test
+    public void testModify(){
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(2L)
+                .title("수정한 제목 연습")
+                .content("수정한 내용 연습")
+                .build();
+
+        boardService.modify(boardDTO);
     }
 }
