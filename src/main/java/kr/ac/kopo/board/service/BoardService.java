b5830e9bce8c29dbb776bb5ebd974d93693ec596
shinjaesang <tlswltkd1011@gmail.com>
2024-10-04 08:57:43 +0900

    수정기능

--- src/main/java/kr/ac/kopo/board/service/BoardService.java
+++ src/main/java/kr/ac/kopo/board/service/BoardService.java
@@ -1,20 +1,46 @@
 package kr.ac.kopo.board.service;
 
 import kr.ac.kopo.board.dto.BoardDTO;
+import kr.ac.kopo.board.dto.PageRequestDTO;
+import kr.ac.kopo.board.dto.PageResultDTO;
 import kr.ac.kopo.board.entity.Board;
 import kr.ac.kopo.board.entity.Member;
 
 public interface BoardService {
-    //새글을 등록하는 기능
+    //    새글을 등록하는 기능
     Long register(BoardDTO dto);
+    //    게시목록 처리 기능
+    PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO);
+    //   특정 게시글 하나를 조회하는 기능
+    BoardDTO get(Long bno);
+    //    삭제 기능
+    void removeWithReplies(Long bno);
+    //    수정 기능
+    void modify(BoardDTO boardDTO);
 
+    //    Entity를 DTO로 변환하는 메소드
+    default BoardDTO entityToDTO(Board board, Member member, Long replyCount){
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(board.getBno())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .regDate(board.getRegDate())
+                .modDate(board.getModDate())
+                .writerEmail(member.getEmail())
+                .writerName(member.getName())
+                .replyCount(replyCount.intValue())
+                .build();
+
+        return boardDTO;
+    }
+
+    //    DTO를 Entity로 변환하는 메소드(
     default Board dtoToEntity(BoardDTO dto){
-
         Member member = Member.builder()
                 .email(dto.getWriterEmail())
                 .build();
 
-        Board board = Board.builder()
+        Board board=Board.builder()
                 .bno(dto.getBno())
                 .title(dto.getTitle())
                 .content(dto.getContent())
--- src/main/java/kr/ac/kopo/board/service/BoardServiceImpl.java
+++ src/main/java/kr/ac/kopo/board/service/BoardServiceImpl.java
@@ -1,22 +1,69 @@
 package kr.ac.kopo.board.service;
 
-
 import kr.ac.kopo.board.dto.BoardDTO;
+import kr.ac.kopo.board.dto.PageRequestDTO;
+import kr.ac.kopo.board.dto.PageResultDTO;
 import kr.ac.kopo.board.entity.Board;
+import kr.ac.kopo.board.entity.Member;
 import kr.ac.kopo.board.repository.BoardRepository;
+import kr.ac.kopo.board.repository.ReplyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
-public class BoardServiceImpl implements BoardService {
+public class BoardServiceImpl implements BoardService{
     private final BoardRepository repository;
+    private final ReplyRepository replyRepository;
+
     @Override
     public Long register(BoardDTO dto) {
         Board board = dtoToEntity(dto);
         repository.save(board);
 
         return board.getBno();
+    }
+
+    @Override
+    public PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO) {
+
+        Function<Object[], BoardDTO> fn = (en -> entityToDTO((Board) en[0], (Member) en[1], (Long) en[2]));
+        Page<Object[]> result = repository.getBoardWithReplyCount(pageRequestDTO.getPageable(Sort.by("bno").descending()));
+
+        return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public BoardDTO get(Long bno) {
+        Object result = repository.getBoardByBno(bno);
+
+        Object[] arr = (Object[]) result;
+        BoardDTO boardDTO = entityToDTO((Board) arr[0], (Member) arr[1], (Long) arr[2]);
+
+        return boardDTO;
+    }
+
+    @Transactional
+    @Override
+    public void removeWithReplies(Long bno) {
+//        댓글삭제
+        replyRepository.deleteByBno(bno);
+//        원글삭제
+        repository.deleteById(bno);
+    }
+
+    @Transactional
+    @Override
+    public void modify(BoardDTO boardDTO) {
+        Board board = repository.getReferenceById(boardDTO.getBno());
+        board.changeTitle(boardDTO.getTitle());
+        board.changeContent(boardDTO.getContent());
+
+        repository.save(board);
     }
 }
