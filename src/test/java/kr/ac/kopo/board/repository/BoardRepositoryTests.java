e30de875626c2d8dd4c76810cd51f39cdcd71383
shinjaesang <tlswltkd1011@gmail.com>
2024-10-25 08:55:26 +0900

    JPQL(Querydsl)을 사용한 검색기능 완성

--- src/test/java/kr/ac/kopo/board/repository/BoardRepositoryTests.java
+++ src/test/java/kr/ac/kopo/board/repository/BoardRepositoryTests.java
@@ -89,8 +89,8 @@ public class BoardRepositoryTests {
 
     @Test
     public void testSearchPage(){
-        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
-        boardRepository.searchPage("t", "5", pageable);
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending().and(Sort.by("title").ascending()));
+        boardRepository.searchPage("t", "1", pageable);
     }
 
 }
