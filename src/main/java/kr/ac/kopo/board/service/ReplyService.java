e94d3380ca01742e4b63594adf5643195f50d2e8
shinjaesang <tlswltkd1011@gmail.com>
2024-11-05 13:51:44 +0900

    ReplyService, ReplyServiceImpl 구현 및 테스트

--- src/main/java/kr/ac/kopo/board/service/ReplyService.java
+++ src/main/java/kr/ac/kopo/board/service/ReplyService.java
@@ -1,9 +1,48 @@
 package kr.ac.kopo.board.service;
 
 import kr.ac.kopo.board.dto.ReplyDTO;
+import kr.ac.kopo.board.entity.Board;
+import kr.ac.kopo.board.entity.Reply;
+
+import java.util.List;
 
 public interface ReplyService {
     //댓글 등록 기능
     Long register(ReplyDTO replyDTO);
+    //댓글 수정 기능
     void modify(ReplyDTO replyDTO);
+    //댓글 삭제 기능
+    void remove(Long rno);
+    //댓글 목록 반환 기능
+    List<ReplyDTO> getList(Long bno);
+    //ReplyDTO => Reply(Entity)
+    default Reply dtoToEntity(ReplyDTO dto){
+
+        Board board = Board.builder()
+                .bno(dto.getBno())
+                .build();
+
+        Reply reply = Reply.builder()
+                .rno(dto.getRno())
+                .text(dto.getText())
+                .replyer(dto.getReplyer())
+                .board(board)
+                .build();
+
+        return reply;
+    }
+
+    //Reply(Entity) => ReplyDTO로 변환
+    default ReplyDTO entityToDto(Reply reply){
+        ReplyDTO replyDTO = ReplyDTO.builder()
+                .rno(reply.getRno())
+                .text(reply.getText())
+                .replyer(reply.getReplyer())
+                .bno(reply.getBoard().getBno())
+                .regDate(reply.getRegDate())
+                .modDate(reply.getModDate())
+                .build();
+
+        return replyDTO;
+    }
 }
